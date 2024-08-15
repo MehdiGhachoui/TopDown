a060787e413d07119a70fff26a66a3fdab86516a
Mehdi Ghachoui <mehdi.ghachoui@onclusive.com>
2024-08-15 23:56:10 +0100

    Start

---
+++ main.lua
@@ -0,0 +1,28 @@
+local sprites = {}
+local player = {}
+
+love.load = function ()
+  sprites.background = love.graphics.newImage("sprites/background.png")
+  sprites.bullet = love.graphics.newImage("sprites/bullet.png")
+  sprites.zombie = love.graphics.newImage("sprites/zombie.png")
+  sprites.player = love.graphics.newImage("sprites/player.png")
+
+  player.x = love.graphics.getWidth()/2
+  player.y = love.graphics.getHeight()/2
+  player.speed = 180 -- <=> moving 3px by frame
+end
+
+love.draw = function ()
+  love.graphics.draw(sprites.background,0,0)
+
+  -- drawing start from the upper left corner of the sprite which means we need to chnage the offset to the center of the sprite
+  -- for rotation change the degree to radian <=> r * pi/180
+  love.graphics.draw(sprites.player,player.x,player.y, math.pi/2,nil,nil,sprites.player:getWidth()/2,sprites.player:getHeight()/2)
+end
+
+love.update = function (dt)
+  -- games in lua run in 60 fps <=> * 1/60  to help in case the frames drop
+  if love.keyboard.isDown("d") then
+    player.x = player.x + player.speed * dt
+  end
+end
